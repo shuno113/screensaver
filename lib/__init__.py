1fb350ef58a7ef0e259fafecc846e38651ac9a45
Shunsuke Ota <shuno113@users.noreply.github.com>
2026-01-01 11:48:11 +0900

    単一ファイルに統合

--- lib/__init__.py
+++
@@ -1,6 +0,0 @@
-# Transporter Library
-from lib.config import *
-from lib.crypto import derive_key_iv, encrypt_plaintext, decrypt_ciphertext
-from lib.header import build_inner_header, build_plaintext_block, parse_inner_header, write_unique_file
-from lib.nibble import nibble_to_gray, gray_to_nibble, nibbles_from_bytes, nibbles_to_gray
-from lib.frame import precompute_data_indices
